0b77673e6e273aa07531fab16614237a58847a09
Dilshanth <dilshanth10@gmail.com>
2018-11-23 00:03:22 +0530

    experience letter ui changes

--- src/app/modules/employee/recruitment/recruitment.module.ts
+++ src/app/modules/employee/recruitment/recruitment.module.ts
@@ -23,8 +23,6 @@ import { SelectInterviewSelectionComponent } from './interview-selection/modal/s
 import { RejectInterviewSelectionComponent } from './interview-selection/modal/reject-interview-selection/reject-interview-selection.component';
 import { AddInterviewPanelComponent } from './interview-history/add-interview-panel/add-interview-panel.component';
 import { ViewInterviewHistoryComponent } from './interview-history/view-interview-history/view-interview-history.component';
-import { ViewInterviewHistoryInternalExternalComponent } from './interview-history/view-interview-history/modal/view-interview-history-internal-external/view-interview-history-internal-external.component';
-import { ViewInterviewHistorySelectedRejectedComponent } from './interview-history/view-interview-history/modal/view-interview-history-selected-rejected/view-interview-history-selected-rejected.component';
 import { ViewInterviewPanelComponent } from './interview-history/view-interview-panel/view-interview-panel.component';
 
 const routes:Routes=[
@@ -58,8 +56,6 @@ const routes:Routes=[
      RejectInterviewSelectionComponent,
      AddInterviewPanelComponent,
      ViewInterviewHistoryComponent,
-     ViewInterviewHistoryInternalExternalComponent,
-     ViewInterviewHistorySelectedRejectedComponent,
      ViewInterviewPanelComponent
 
     ]
